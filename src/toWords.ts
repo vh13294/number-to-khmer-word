f878f98411def90ec17d812e86c629f5233cdcb7
vh13294 <huotvongpanha@gmail.com>
2020-01-18 23:33:51 +0700

    First commit

---
+++ src/toWords.ts
@@ -0,0 +1,60 @@
+const SINGLE_DIGIT = ['សូន្យ', 'មួយ', 'ពីរ', 'បី', 'បួន', 'ប្រាំ', 'ប្រាំមួយ', 'ប្រាំពីរ', 'ប្រាំបី', 'ប្រាំបួន'];
+const DOUBLE_DIGIT = ['', 'ដប់', 'ម្ភៃ', 'សាមសិប', 'សែសិប', 'ហាសិប', 'ហុកសិប', 'ចិតសិប', 'ប៉ែតសិប', 'កៅសិប'];
+
+const TEN = 10;
+const ONE_HUNDRED = 100;
+const ONE_THOUSAND = 1000;
+const TEN_THOUSAND = 10_000;
+const ONE_HUNDRED_THOUSAND = 100_000;
+const ONE_MILLION = 1000_000;
+const ONE_BILLION = 1000_000_000;
+const ONE_TRILLION = 1000_000_000_000;
+const ONE_QUADRILLION = 1000_000_000_000_000n;
+
+const seperator = '-';
+
+export function toWords(input: number|string): string {
+    let word = '';
+    let remainder = 0;
+    const num = Number(input);
+
+    if(num < TEN) {
+        word = SINGLE_DIGIT[num];
+
+    } else if(num < ONE_HUNDRED) {
+        remainder = num % TEN;
+        word = DOUBLE_DIGIT[Math.floor(num/TEN)];
+
+    } else if(num < ONE_THOUSAND) {
+        remainder = num % ONE_HUNDRED;
+        word = SINGLE_DIGIT[Math.floor(num/ONE_HUNDRED)] + 'រយ';
+        
+    } else if(num < TEN_THOUSAND) {
+        remainder = num % ONE_THOUSAND;
+        word = SINGLE_DIGIT[Math.floor(num/ONE_THOUSAND)] + 'ពាន់';
+
+    } else if(num < ONE_HUNDRED_THOUSAND) {
+        remainder = num % TEN_THOUSAND;
+        word = SINGLE_DIGIT[Math.floor(num/TEN_THOUSAND)] + 'ម៉ឺន';
+
+    } else if(num < ONE_MILLION) {
+        remainder = num % ONE_HUNDRED_THOUSAND;
+        word = SINGLE_DIGIT[Math.floor(num/ONE_HUNDRED_THOUSAND)] + 'សែន';
+
+    } else if(num < ONE_BILLION) {
+        remainder = num % ONE_MILLION;
+        word = toWords(Math.floor(num/ONE_MILLION)) + 'លាន';
+
+    } else if(num < ONE_TRILLION) {
+        remainder = num % ONE_BILLION;
+        word = toWords(Math.floor(num/ONE_BILLION)) + 'ប៊ីលាន';
+
+    } else if(num < ONE_QUADRILLION) {
+        remainder = num % ONE_TRILLION;
+        word = toWords(Math.floor(num/ONE_TRILLION)) + 'ទ្រីលាន';
+    }
+
+    if(remainder)
+        return `${word}${seperator}${toWords(remainder)}`;
+    return word;
+}
---
+++ test/toWords.ts
@@ -0,0 +1,65 @@
+import { toWords } from "./../src/toWords";
+
+// console.log(toWords('0'));
+// console.log(toWords('1'));
+// console.log(toWords('9'));
+
+// console.log('\n');
+
+// console.log(toWords('90'));
+// console.log(toWords('91'));
+// console.log(toWords('99'));
+
+// console.log('\n');
+
+// console.log(toWords('199'));
+// console.log(toWords('800'));
+// console.log(toWords('999'));
+
+// console.log('\n');
+
+// console.log(toWords('1000'));
+// console.log(toWords('1999'));
+// console.log(toWords('9999'));
+
+// console.log('\n');
+
+// console.log(toWords(10_000));
+// console.log(toWords(29_999));
+// console.log(toWords(99_999));
+
+// console.log('\n');
+
+// console.log(toWords(100_000));
+// console.log(toWords(299_999));
+// console.log(toWords(999_999));
+
+// console.log('\n');
+
+// console.log(toWords(1_000_000));
+// console.log(toWords(2_999_999));
+// console.log(toWords(9_999_999));
+
+// console.log('\n');
+
+// console.log(toWords(10_000_000));
+// console.log(toWords(29_999_999));
+// console.log(toWords(99_999_999));
+
+// console.log('\n');
+
+// console.log(toWords(100_000_000));
+// console.log(toWords(299_999_999));
+// console.log(toWords(999_999_999));
+
+console.log('\n');
+
+console.log(toWords(1_000_000_000));
+console.log(toWords(2_999_999_999));
+console.log(toWords(9_999_999_999));
+
+console.log('\n');
+
+console.log(toWords(1_000_000_000_000));
+console.log(toWords(2_999_999_999_999));
+console.log(toWords(9_999_999_999_999));
